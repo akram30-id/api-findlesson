9db77c6a0badefaa0e52b75a857e2d0a4059cc36
akram30-id <afalerin2016@gmail.com>
2024-08-27 16:19:11 +0700

    add: reset & update password

--- src/controller/user-controller.js
+++ src/controller/user-controller.js
@@ -37,8 +37,37 @@ const logout = async (req, res, next) => {
     }
 }
 
+const updatePassword = async (req, res, next) => {
+    try {
+        const update = await userService.updatePassword(req.body, req.user);
+
+        res.status(200).json({
+            data: update
+        });
+    } catch (e) {
+        next(e);
+    }
+}
+
+const resetPassword = async (req, res , next) => {
+    try {
+        const username = req.body.username;
+        const role = req.user.role;
+
+        const update = await userService.resetPassword(username, role);
+
+        res.status(200).json({
+            data: update
+        });
+    } catch (e) {
+        next(e)
+    }
+}
+
 export default {
     register,
     login,
-    logout
+    logout,
+    updatePassword,
+    resetPassword
 }
--- src/service/user-service.js
+++ src/service/user-service.js
@@ -1,9 +1,10 @@
 import { validate } from "../validation/validation.js"
 import { prismaClient } from "../application/database.js"
 import { ResponseError } from "../error/response-error.js";
-import { getUserValidation, loginUserValidation, registerUserValidation } from "../validation/user-validation.js";
+import { getUserValidation, loginUserValidation, registerUserValidation, updatePasswordValidation } from "../validation/user-validation.js";
 import bcrypt from "bcrypt";
 import { v4 as uuid } from "uuid";
+import e from "express";
 
 const register = async (request) => {
     const user = validate(registerUserValidation, request);
@@ -101,8 +102,98 @@ const logout = async (username) => {
     });
 }
 
+const updatePassword = async (request, user) => {
+    request = validate(updatePasswordValidation, request);
+ 
+    const validateUser = await prismaClient.user.findFirst({
+        where: {
+            token: user.token
+        },
+        select: {
+            username: true
+        }
+    });
+
+    if (!validateUser) {
+        throw new ResponseError(404, "User is not found.");
+    }
+
+    if (validateUser.username !== request.username) {
+        throw new ResponseError(403, "Invalid user!");
+    }
+
+    const encryptOldPwd = bcrypt.hash(request.old_password, 10);
+
+    const validateOldPassword = await prismaClient.user.findFirst({
+        where: {
+            username: request.username,
+            password: encryptOldPwd
+        }
+    });
+
+    if (!validateOldPassword) {
+        throw new ResponseError(403, "Invalid old password");
+    }
+
+    if (request.new_password !== request.confirm_password) {
+        throw new ResponseError(400, "New Password and Confirmation Password is not match");
+    }
+
+    const encryptNewPwd = await bcrypt.hash(request.new_password, 10);
+
+    return prismaClient.user.update({
+        where: {
+            username: request.username
+        },
+        data: {
+            password: encryptNewPwd
+        },
+        select: {
+            id: true,
+            username: true,
+            email: true
+        }
+    });
+}
+
+const resetPassword = async (username, role) => {
+    username = validate(getUserValidation, username);
+
+    const getUser = await prismaClient.user.findFirst({
+        where: {
+            username: username
+        }
+    });
+
+    if (!getUser) {
+        throw new ResponseError(404, "User is not found.");
+    }
+
+    if (role == 'admin' && (getUser.role == 'superadmin' || getUser.role == 'admin')) {
+        throw new ResponseError(403, "Unauthorized previllage.");
+    }
+
+    const newPassword = await bcrypt.hash('default', 10);
+
+    return prismaClient.user.update({
+        where: {
+            username: username
+        },
+        data: {
+            password: newPassword
+        },
+        select: {
+            id: true,
+            email: true,
+            username: true
+        }
+    });
+}
+
 export default {
     register,
     login,
-    logout
+    logout,
+    updatePassword,
+    resetPassword
 }
